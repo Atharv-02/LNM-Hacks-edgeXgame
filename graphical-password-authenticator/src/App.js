18124c79ef6e05223e7138c7d2ea48a733bc5ccb
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-01-20 11:31:39 +0530

    new

---
+++ graphical-password-authenticator/src/App.js
@@ -0,0 +1,229 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Modal';
+import Modal from './components/Modal';
+import { useState } from 'react';
+import postData from './utils/postData';
+import getData from './utils/getData';
+import sha256 from './utils/hashing';
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    // Generate random number
+    var j = Math.floor(Math.random() * (i + 1));
+
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+}
+const elementsSignUp = shuffleArray([...Array(28).keys()]);
+const elementsLogin = shuffleArray([...Array(28).keys()]);
+
+function App() {
+  const [conditionSignUp, setConditionSignUp] = useState(false);
+  const [conditionLogin, setConditionLogin] = useState(false);
+  const [signUpPassword, setSignUpPassword] = useState(false);
+  const [loginPassword, setLoginPassword] = useState(false);
+
+  const [selectedArraySignUp, setSelectedArraySignUp] = useState([]);
+  const [selectedArrayLogin, setSelectedArrayLogin] = useState([]);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [email2, setEmail2] = useState('');
+
+  const onClickHandleSignup = (e) => {
+    if (conditionSignUp) {
+      setConditionSignUp(false);
+    } else {
+      setConditionSignUp(true);
+    }
+  };
+
+  const onClickHandleLogin = (e) => {
+    if (conditionSignUp) {
+      setConditionLogin(false);
+    } else {
+      setConditionLogin(true);
+    }
+  };
+
+  return (
+    <>
+      {conditionSignUp ? (
+        <Modal
+          disappearFucntion={setConditionSignUp}
+          checkPassword={setSignUpPassword}
+          elements={elementsSignUp}
+          selectedArray={selectedArraySignUp}
+          setSelectedArray={setSelectedArraySignUp}
+        />
+      ) : (
+        ''
+      )}
+      {conditionLogin ? (
+        <Modal
+          disappearFucntion={setConditionLogin}
+          checkPassword={setLoginPassword}
+          elements={elementsLogin}
+          selectedArray={selectedArrayLogin}
+          setSelectedArray={setSelectedArrayLogin}
+        />
+      ) : (
+        ''
+      )}
+      <div className='section'>
+        <div className='container'>
+          <div className='row full-height justify-content-center'>
+            <div className='col-12 text-center align-self-center py-5'>
+              <div className='section pb-5 pt-5 pt-sm-2 text-center'>
+                <h6 className='mb-0 pb-3'>
+                  <span>Log In </span>
+                  <span>Sign Up</span>
+                </h6>
+                <input
+                  className='checkbox'
+                  type='checkbox'
+                  id='reg-log'
+                  name='reg-log'
+                />
+                <label htmlFor='reg-log'></label>
+                <div className='card-3d-wrap mx-auto'>
+                  <div className='card-3d-wrapper'>
+                    <div className='card-front'>
+                      <div className='center-wrap'>
+                        <div className='section text-center'>
+                          <h4 className='mb-4 pb-3'>Log In</h4>
+                          <div className='form-group'>
+                            <input
+                              type='email'
+                              name='logemail'
+                              className='form-style'
+                              placeholder='Your Email'
+                              id='logemail'
+                              autoComplete='off'
+                              value={email2}
+                              onChange={(e) => setEmail2(e.target.value)}
+                            />
+                            <i className='input-icon uil uil-at'></i>
+                          </div>
+                          <div className='form-group mt-2'>
+                            <div
+                              className={
+                                loginPassword
+                                  ? 'form-style connected'
+                                  : 'form-style'
+                              }
+                              id='logpass'
+                              onClick={onClickHandleLogin}
+                            >
+                              {' '}
+                              {loginPassword
+                                ? 'Password added successfully !'
+                                : 'Click Here to set up Password'}
+                            </div>
+                            <i className='input-icon uil uil-lock-alt'></i>
+                          </div>
+                          <a
+                            href='#'
+                            className='btn mt-4'
+                            onClick={() => {
+                              sha256(selectedArrayLogin.join(''))
+                                .then((data) => {
+                                  getData(email2, data);
+                                })
+                                .catch();
+                            }}
+                          >
+                            submit
+                          </a>
+                          <p className='mb-0 mt-4 text-center'>
+                            <a href='#0' className='link'>
+                              Forgot your password?
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='card-back'>
+                      <div className='center-wrap'>
+                        <div className='section text-center'>
+                          <h4 className='mb-4 pb-3'>Sign Up</h4>
+                          <div className='form-group'>
+                            <input
+                              type='text'
+                              name='logname'
+                              className='form-style'
+                              placeholder='Your Full Name'
+                              value={name}
+                              onChange={(e) => {
+                                setName(e.target.value);
+                              }}
+                              id='logname'
+                              autoComplete='off'
+                            />
+                            <i className='input-icon uil uil-user'></i>
+                          </div>
+                          <div className='form-group mt-2'>
+                            <input
+                              type='email'
+                              name='logemail'
+                              className='form-style'
+                              placeholder='Your Email'
+                              value={email}
+                              onChange={(e) => {
+                                setEmail(e.target.value);
+                              }}
+                              id='logemail'
+                              autoComplete='off'
+                            />
+                            <i className='input-icon uil uil-at'></i>
+                          </div>
+                          <div className='form-group mt-2'>
+                            <div
+                              className={
+                                signUpPassword
+                                  ? 'form-style connected'
+                                  : 'form-style'
+                              }
+                              id='logpass'
+                              onClick={onClickHandleSignup}
+                            >
+                              {' '}
+                              {signUpPassword
+                                ? 'Password added successfully !'
+                                : 'Click Here to set up Password'}
+                            </div>
+                            <i className='input-icon uil uil-lock-alt'></i>
+                          </div>
+                          <a
+                            href='#'
+                            className='btn mt-4'
+                            onClick={() => {
+                              sha256(selectedArraySignUp.join(''))
+                                .then((data) => {
+                                  postData(email, data);
+                                })
+                                .catch();
+                            }}
+                          >
+                            submit
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
